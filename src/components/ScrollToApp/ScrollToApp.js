249a06f35a772c559609ba702578bbbddd288141
DevSrabon <et.srabon@gmail.com>
2023-03-15 23:27:43 +0600

    change color of whatsapp icon

--- src/components/ScrollToApp/ScrollToApp.js
+++ src/components/ScrollToApp/ScrollToApp.js
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { BsWhatsapp } from "react-icons/bs";
 const ScrollToApp = () => {
 	const [showScrollApp, setShowScrollApp] = useState(false);
 
     useEffect(() => {
         window.addEventListener('scroll', () => { 
 
-            if (window.scrollY > 100) {
+            if (window.scrollY > 400) {
                 setShowScrollApp(true);
             } else {
                 setShowScrollApp(false);
             }
         })
 	}, []);
-	return <>{showScrollApp && <BsWhatsapp className="btn-position"/>}</>;
+    return <>{showScrollApp && <div className="btn-position"><img src="https://www.wanderon.in/svg/call-icon/whatsapp.svg" alt="" /></div>}</>;
 };
 
 export default ScrollToApp;
